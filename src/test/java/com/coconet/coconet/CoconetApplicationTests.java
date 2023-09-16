3658a7991b356e3de37a31d445ffdcd2021d156e
Jeremy <jamkim1997@gmail.com>
2023-09-17 00:54:16 +1000

    init : 프로젝트 생성

---
+++ src/test/java/com/coconet/coconet/CoconetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coconet.coconet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoconetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
