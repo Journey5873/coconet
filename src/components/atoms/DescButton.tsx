8613d928892a01b88db8886d83a08750549be5f8
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2023-09-27 21:59:19 +0900

    feat : DescButton 수정

--- src/components/atoms/DescButton.tsx
+++ src/components/atoms/DescButton.tsx
@@ -4,28 +4,19 @@ import styled from 'styled-components';
 
 interface Props {
     label: string;
-    color?: string;
     onClick?: () => void;
-    bgColor?: string;
 }
 
-const DescButton: React.FC<Props> = ({ label, onClick, color = 'black', bgColor = 'lightgray' }) => {
-    return (
-        <StyledDescButton onClick={onClick} color={color} bgColor={bgColor}>
-            {label}
-        </StyledDescButton>
-    );
+const DescButton: React.FC<Props> = ({ label, onClick }) => {
+    return <StyledDescButton onClick={onClick}>{label}</StyledDescButton>;
 };
 
 export default DescButton;
 
-const StyledDescButton = styled.button<{
-    color: string;
-    bgColor: string;
-}>`
+const StyledDescButton = styled.button`
     padding: 4px 12px;
     border: none;
     border-radius: 1rem;
-    color: ${(props) => props.color};
-    background-color: ${(props) => props.bgColor};
+    color: dodgerblue;
+    background-color: lightgray;
 `;
