0eb68b01109827efb69ea6935faa7c7167e6b7fd
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2023-09-27 14:40:19 +0900

    feat : SpecButton

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import Dday from './components/atoms/Dday';
 import Division from './components/atoms/Division';
 import Endline from './components/atoms/EndLine';
 
+import SpecButton from './components/atoms/Button';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>코코넷</h1>
-      <Dday dDay={7} />
-      <Division username={'사계'} registeredDate={'2023.09.12'} />
-      <Endline endline={'2023.09.30'} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>코코넷</h1>
+            <Dday dDay={7} />
+            <Division username={'사계'} registeredDate={'2023.09.12'} />
+            <Endline endline={'2023.09.30'} />
+            <SpecButton iconUrl="#" label="React" />
+        </div>
+    );
 }
 
 export default App;
