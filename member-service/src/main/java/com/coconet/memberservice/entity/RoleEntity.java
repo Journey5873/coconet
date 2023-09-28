87f9d120e6c9b5b35a0b23622d941cd9016c5343
Seong57 <Seong57@naver.com>
2023-09-28 17:03:32 +0900

    add : role entity

---
+++ member-service/src/main/java/com/coconet/memberservice/entity/RoleEntity.java
@@ -0,0 +1,29 @@
+package com.coconet.memberservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "role")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
