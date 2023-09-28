644a82c017018b5ebe87f1080b893ac79678ae90
Seong57 <Seong57@naver.com>
2023-09-28 19:27:56 +0900

    add : health call

---
+++ member-service/src/main/java/com/coconet/memberservice/health/HealthController.java
@@ -0,0 +1,40 @@
+package com.coconet.memberservice.health;
+
+import com.coconet.common.response.Response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/open-api/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public Response<HealthResponse> health(){
+
+        HealthResponse response = HealthResponse.builder()
+                .id(1L)
+                .name("coconet")
+                .email("coconet@coconet.com")
+                .build();
+
+        return Response.OK(response);
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class HealthResponse {
+
+        private Long id;
+        private String name;
+        private String email;
+    }
+
+
+}
