00ada35bfff4132cef382afb1fb09d354add0bf5
Seong57 <Seong57@naver.com>
2023-09-30 05:04:43 +0900

    add : MemberRepository findByEmail() 추가 후 커밋

---
+++ member-service/src/main/java/com/coconet/memberservice/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.coconet.memberservice.repository;
+
+import com.coconet.memberservice.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findByEmail(String email);
+}
