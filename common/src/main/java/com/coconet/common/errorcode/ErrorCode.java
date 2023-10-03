7fe9f166b89763f8f66204da6ffe44b654efb816
Seong57 <Seong57@naver.com>
2023-10-04 00:10:04 +0900

    edit : add NULL_POINT error

--- common/src/main/java/com/coconet/common/errorcode/ErrorCode.java
+++ common/src/main/java/com/coconet/common/errorcode/ErrorCode.java
@@ -2,6 +2,7 @@ package com.coconet.common.errorcode;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
@@ -10,6 +11,7 @@ public enum ErrorCode implements ErrorCodeIfs{
     OK(200, 200, "성공"),
     BAD_REQUEST(400, 400, "잘못된 요청"),
     SERVER_ERROR(500, 500, "서버 내 오류"),
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "null point"),
     ;
     private final Integer httpStatusCode;   // 클라이언트에 보여줄 status code
     private final Integer errorCode;    // 우리 서버 내부에서 사용할 에러 코드
