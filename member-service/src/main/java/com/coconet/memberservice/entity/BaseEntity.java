81b0fd41f65db5b73c1e52a70a02f08dbee1a8a3
inna <journeyin73@gmail.com>
2023-10-05 10:26:55 +0900

    edit: Auditing 추가

--- member-service/src/main/java/com/coconet/memberservice/entity/BaseEntity.java
+++ member-service/src/main/java/com/coconet/memberservice/entity/BaseEntity.java
@@ -1,10 +1,15 @@
 package com.coconet.memberservice.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -12,11 +17,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseEntity {
 
-    @Column(nullable = false)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
