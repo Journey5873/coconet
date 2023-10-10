9fc42be48dce920d2cffca7fa43183de22a89dd6
Jeremy <jamkim1997@gmail.com>
2023-10-11 01:30:14 +1100

    Remove : unnecessary Dtos deleted Edit : change data type from multipartFile to String for profileImg to show the path

--- member-service/src/main/java/com/coconet/memberservice/dto/MemberResponseDto.java
+++ member-service/src/main/java/com/coconet/memberservice/dto/MemberResponseDto.java
@@ -1,18 +1,20 @@
 package com.coconet.memberservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Builder
 @AllArgsConstructor
 @Getter
 public class MemberResponseDto {
     private String name;
     private int career;
-    private MultipartFile profileImg;
+    private String profileImg;
     private List<String> roles;
     private List<String> stacks;
 }
