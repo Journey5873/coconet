c5a16f92e87e12dcf6bf36851a0bf473d148569e
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2023-10-13 19:41:37 +0900

    feat : useFecth
    데이터 패칭하는 훅
    
    pages/index.tsx 에 간단한 사용 예시

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,67 @@
+import { useState, useEffect, useMemo } from "react";
+
+const setItemWithExpireTime = <T extends any[]>(keyName: string, keyValue: T, tts: number) => {
+    const now = new Date();
+    const obj = {
+        value: keyValue,
+        expire: now.getTime() + tts,
+    };
+
+    const objString = JSON.stringify(obj);
+
+    localStorage.setItem(keyName, objString);
+};
+
+export default function useFetch<T extends any[]>(fetchFn: () => Promise<T>, name: string, initialState: T) {
+    const [data, setData] = useState<T>(initialState);
+    const [isError, setIsError] = useState<boolean>(false);
+    const [page, setPage] = useState<number>(1);
+    const [lastPage, setLastPage] = useState<number>(0);
+
+    const handlePage = (value: number) => {
+        if (value == 1) getState(0);
+
+        if (value !== 1) getState(value * 10);
+
+        setPage(value);
+    };
+
+    const getState = async (sliceStartPage: number) => {
+        try {
+            const now = new Date();
+            const cacheData = localStorage.getItem(name);
+
+            if (cacheData) {
+                const parseData = JSON.parse(cacheData);
+
+                if (now.getTime() < parseData.expire) {
+                    setData(parseData.value.slice(sliceStartPage, sliceStartPage + 10) as T);
+                    setIsError(false);
+                    return;
+                }
+
+                localStorage.removeItem(name);
+            }
+            const result = await fetchFn();
+
+            setLastPage(Math.ceil([result].length / 10));
+            setItemWithExpireTime(name, [result], 1000 * 60 * 10);
+            setData([result].slice(sliceStartPage, sliceStartPage + 10) as T);
+            setIsError(false);
+        } catch (error) {
+            setIsError(true);
+        }
+    };
+
+    useEffect(() => {
+        getState(0);
+    }, []);
+
+    return {
+        data,
+        isError,
+        page,
+        lastPage,
+        handlePage,
+    };
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 
+import useFetch from "../hooks/useFetch";
+
+interface Dummy {
+    count: number;
+    name: string;
+    age: number;
+}
+
 const Index = () => {
+    const getData = async () => {
+        const response = await fetch("https://api.agify.io?name=michael");
+        const result = await response.json();
+
+        return result;
+    };
+    const { data } = useFetch<Dummy[]>(getData, "dummy", []);
+
+    console.log(data);
+
     return <div>hello world</div>;
 };
 
