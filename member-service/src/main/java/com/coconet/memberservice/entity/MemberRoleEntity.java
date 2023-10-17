75f344c73389262f4adb9b14fd41c8b044f5d24e
inna <journeyin73@gmail.com>
2023-10-18 03:50:11 +0900

    Edit: @ManyToOne to @ManyToOne(fetch = FetchType.LAZY)

--- member-service/src/main/java/com/coconet/memberservice/entity/MemberRoleEntity.java
+++ member-service/src/main/java/com/coconet/memberservice/entity/MemberRoleEntity.java
@@ -22,11 +22,16 @@ public class MemberRoleEntity extends BaseEntity{
     @Column(name = "member_role_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private MemberEntity member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id")
     private RoleEntity role;
+
+    public MemberRoleEntity(MemberEntity member, RoleEntity roleEntity){
+        this.member = member;
+        this.role = roleEntity;
+    }
 }
