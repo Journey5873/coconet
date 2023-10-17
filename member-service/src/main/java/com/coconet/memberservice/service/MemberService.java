7120c161e3e3fa19a46ebaf57e9f968b4af871f1
Jeremy <jamkim1997@gmail.com>
2023-10-18 00:27:58 +1100

    Edit : optimised the codes for better performance of JPA

--- member-service/src/main/java/com/coconet/memberservice/service/MemberService.java
+++ member-service/src/main/java/com/coconet/memberservice/service/MemberService.java
@@ -1,22 +1,20 @@
 package com.coconet.memberservice.service;
 
+import com.coconet.memberservice.common.errorcode.ErrorCode;
+import com.coconet.memberservice.common.exception.ApiException;
 import com.coconet.memberservice.dto.MemberRequestDto;
 import com.coconet.memberservice.dto.MemberResponseDto;
 import com.coconet.memberservice.entity.*;
 import com.coconet.memberservice.repository.*;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.time.LocalDateTime;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,13 +27,17 @@ public class MemberService {
     private final TechStackRepository techStackRepository;
     private final MemberStackRepository memberStackRepository;
 
-
     public MemberResponseDto getUserInfo(Long id){
         MemberEntity member = memberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("No member found"));
+                .orElseThrow(() -> new ApiException(ErrorCode.NOT_FOUND));
 
-        List<String> returnRoles = getAllRoles(id);
-        List<String> returnStacks = getAllStacks(id);
+        List<String> returnRoles = getAllRoles(member).stream()
+                .map(RoleEntity::getName)
+                .toList();
+        List<String> returnStacks = getAllStacks(member).stream()
+                .map(TechStackEntity::getName)
+                .toList();
+
         return MemberResponseDto.builder()
                 .name(member.getName())
                 .career(Integer.parseInt(member.getCareer()))
@@ -48,49 +50,19 @@ public class MemberService {
     public MemberResponseDto updateUserInfo(Long id, MemberRequestDto requestDto, MultipartFile imageFile) {
 
         MemberEntity member = memberRepository.findById(id)
-                                                    .orElseThrow(() -> new IllegalArgumentException("No member found"));
+                                                    .orElseThrow(() -> new ApiException(ErrorCode.NOT_FOUND));
 
-        try {
-            member.changeName(requestDto.getName());
-            member.changeCareer(String.valueOf(requestDto.getCareer()));
-            member.changeProfileImage(updateProfilePic(id, imageFile));
-            member.changeGithubLink(requestDto.getGithubLink());
-            member.changeBlogLink(requestDto.getBlogLink());
-            member.changeNotionLink(requestDto.getNotionLink());
-
-            MemberEntity returnMember = memberRepository.save(member);
-
-            List<String> returnRoles = updateRoles(id, requestDto.getRoles());
-            List<String> returnStacks = updateStacks(id, requestDto.getStacks());
-
-            return MemberResponseDto.builder()
-                    .name(returnMember.getName())
-                    .career(Integer.parseInt(returnMember.getCareer()))
-                    .profileImg(returnMember.getProfileImage())
-                    .roles(returnRoles)
-                    .stacks(returnStacks)
-                    .githubLink(returnMember.getGithubLink())
-                    .blogLink(returnMember.getBlogLink())
-                    .notionLink(returnMember.getNotionLink())
-                    .build();
-        }
-        catch(Exception e) {
-            System.out.println(e);
-            new IllegalArgumentException("Update fails");
-            return null;
-        }
-    }
-
-    public MemberResponseDto deleteUser(Long id) {
-        MemberEntity member = memberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("No member found"));
-
-        member.deleteUser();
+        member.changeName(requestDto.getName());
+        member.changeCareer(String.valueOf(requestDto.getCareer()));
+        member.changeProfileImage(updateProfilePic(member, imageFile));
+        member.changeGithubLink(requestDto.getGithubLink());
+        member.changeBlogLink(requestDto.getBlogLink());
+        member.changeNotionLink(requestDto.getNotionLink());
 
         MemberEntity returnMember = memberRepository.save(member);
 
-        List<String> returnRoles = getAllRoles(id);
-        List<String> returnStacks = getAllStacks(id);
+        List<String> returnRoles = updateRoles(member, requestDto.getRoles());
+        List<String> returnStacks = updateStacks(member, requestDto.getStacks());
 
         return MemberResponseDto.builder()
                 .name(returnMember.getName())
@@ -104,91 +76,114 @@ public class MemberService {
                 .build();
     }
 
-    public List<String> getAllRoles(Long memberId) {
-        List<MemberRoleEntity> ids = memberRoleRepository.findAllByMemberId(memberId);
+    public MemberResponseDto deleteUser(Long id) {
+        MemberEntity member = memberRepository.findById(id)
+                .orElseThrow(() -> new ApiException(ErrorCode.NOT_FOUND));
+
+        member.deleteUser();
+
+        MemberEntity returnMember = memberRepository.save(member);
+
+        List<String> returnRoles = getAllRoles(member).stream()
+                .map(RoleEntity::getName)
+                .toList();
+        List<String> returnStacks = getAllStacks(member).stream()
+                .map(TechStackEntity::getName)
+                .toList();
+
+        return MemberResponseDto.builder()
+                .name(returnMember.getName())
+                .career(Integer.parseInt(returnMember.getCareer()))
+                .profileImg(returnMember.getProfileImage())
+                .roles(returnRoles)
+                .stacks(returnStacks)
+                .githubLink(returnMember.getGithubLink())
+                .blogLink(returnMember.getBlogLink())
+                .notionLink(returnMember.getNotionLink())
+                .build();
+    }
+
+    public List<RoleEntity> getAllRoles(MemberEntity member) {
+        List<MemberRoleEntity> ids = memberRoleRepository.findAllByMemberId(member.getId());
+
         if(ids.size() == 0) {
-            throw new IllegalArgumentException("");
+            throw new ApiException(ErrorCode.BAD_REQUEST);
         }
 
-        List<String> returnRoles = new ArrayList<>();
+        List<RoleEntity> returnRoles = new ArrayList<>();
         for (MemberRoleEntity memberRoleEntity : ids) {
-            returnRoles.add(memberRoleEntity.getRole().getName());
+            returnRoles.add(memberRoleEntity.getRole());
         }
         return returnRoles;
     }
 
-    public List<String> getAllStacks(Long memberId){
-        List<MemberStackEntity> ids = memberStackRepository.findByMemberId(memberId);
+    public List<TechStackEntity> getAllStacks(MemberEntity member){
+        List<MemberStackEntity> ids = memberStackRepository.findByMemberId(member.getId());
+
         if(ids.size() == 0) {
-            throw new IllegalArgumentException("");
+            throw new ApiException(ErrorCode.BAD_REQUEST);
         }
 
-        List<String> returnStacks = new ArrayList<>();
+        List<TechStackEntity> returnStacks = new ArrayList<>();
         for (MemberStackEntity memberStack : ids) {
-            returnStacks.add(memberStack.getTechStack().getName());
+            returnStacks.add(memberStack.getTechStack());
         }
         return returnStacks;
     }
 
-    public String updateProfilePic(Long id, MultipartFile image) throws Exception {
-        MemberEntity member = memberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("No member found"));
+    public String updateProfilePic(MemberEntity member, MultipartFile image) {
 
         String imagePath = null;
         String absolutePath = new File("").getAbsolutePath() + "/";
         String path = "member-service/src/main/resources/memberProfilePics";
 
         if (!image.isEmpty()) {
-            imagePath = path + "/" + id + ".png";
+            imagePath = path + "/" + member.getId() + ".png";
             File file = new File(absolutePath + imagePath);
-            image.transferTo(file);
+
+            try {
+                image.transferTo(file);
+            } catch (IOException e) {
+                throw new ApiException(ErrorCode.SERVER_ERROR);
+            }
 
             member.changeProfileImage(imagePath);
             memberRepository.save(member);
         } else {
-            File previousFile = new File(absolutePath + path + "/" + id + ".png");
+            File previousFile = new File(absolutePath + path + "/" + member.getId() + ".png");
             if(previousFile.exists())
                 previousFile.delete();
             return path + "/basic_image.png";
         }
-
         return imagePath;
     }
 
-    public List<String> updateRoles(Long memberId, List<String> roles) {
-        MemberEntity member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+    public List<String> updateRoles(MemberEntity member, List<String> roles) {
 
-        List<Long> inputRoles = roles.stream()
+        List<RoleEntity> inputRoles = roles.stream()
                 .map(roleName -> roleRepository.findByName(roleName)
-                        .orElseThrow(() -> new IllegalArgumentException("Role not found: " + roleName))
+                        .orElseThrow(() -> new ApiException(ErrorCode.NOT_FOUND))
                 )
-                .map(RoleEntity::getId)
                 .toList();
 
         // Get current roles
-        List<Long> currentRoles = memberRoleRepository.findByMemberId(memberId)
-                .stream()
-                .map(MemberRoleEntity -> MemberRoleEntity.getRole().getId())
-                .toList();
+        List<RoleEntity> currentRoles = getAllRoles(member);
 
         // Identify new roles to add
-        List<Long> rolesToAdd = inputRoles.stream()
+        List<RoleEntity> rolesToAdd = inputRoles.stream()
                 .filter(roleId -> !currentRoles.contains(roleId))
                 .toList();
 
         // Identify roles to remove
-        List<Long> rolesToRemove = currentRoles.stream()
+        List<RoleEntity> rolesToRemove = currentRoles.stream()
                 .filter(currRoleId -> !inputRoles.contains(currRoleId))
                 .toList();
 
         // Create MemberRoleEntity to add
         List<MemberRoleEntity> memberRoleEntitiesToAdd = rolesToAdd.stream()
-                .map(roleId -> MemberRoleEntity.builder()
+                .map(role -> MemberRoleEntity.builder()
                         .member(member)
-                        .role(roleRepository.findById(roleId)
-                                .orElseThrow(() -> new IllegalArgumentException("No role exists"))
-                        )
+                        .role(role)
                         .build()
                 )
                 .collect(Collectors.toList());
@@ -196,51 +191,45 @@ public class MemberService {
 
         // Get MemberRoleEntity to remove
         List<MemberRoleEntity> memberRoleEntitiesToRemove = rolesToRemove.stream()
-                .map(roleId -> memberRoleRepository.findByMemberIdAndRoleId(memberId, roleId)
-                        .orElseThrow(() -> new IllegalArgumentException("No Role"))
+                .map(role -> MemberRoleEntity.builder()
+                        .member(member)
+                        .role(role)
+                        .build()
                 )
                 .collect(Collectors.toList());
+
         memberRoleRepository.deleteAllInBatch(memberRoleEntitiesToRemove);
 
-        return memberRoleRepository.findByMemberId(memberId).stream()
+        return memberRoleRepository.findByMemberId(member.getId()).stream()
                 .map(memberRoleEntity -> memberRoleEntity.getRole().getName())
                 .toList();
     }
 
-    List<String> updateStacks(Long memberId, List<String> stacks){
-        MemberEntity member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
-
-        List<Long> stacksId = stacks.stream()
+    List<String> updateStacks(MemberEntity member, List<String> stacks){
+        List<TechStackEntity> stacksId = stacks.stream()
                 .map(stackName -> techStackRepository.findByName(stackName)
-                        .orElseThrow(() -> new IllegalArgumentException("Stack not found: " + stackName))
+                        .orElseThrow(() -> new ApiException(ErrorCode.NOT_FOUND))
                 )
-                .map(TechStackEntity::getId)
                 .toList();
 
         // Get current stacks
-        List<Long> currentStacks = memberStackRepository.findByMemberId(memberId)
-                .stream()
-                .map(MemberStackEntity -> MemberStackEntity.getTechStack().getId())
-                .toList();
+        List<TechStackEntity> currentStacks = getAllStacks(member);
 
         // Identify new stacks to add
-        List<Long> stacksToAdd = stacksId.stream()
-                .filter(stackId -> !currentStacks.contains(stackId))
+        List<TechStackEntity> stacksToAdd = stacksId.stream()
+                .filter(stack -> !currentStacks.contains(stack))
                 .toList();
 
         // Identify stacks to remove
-        List<Long> stacksToRemove = currentStacks.stream()
-                .filter(currStackId -> !stacksId.contains(currStackId))
+        List<TechStackEntity> stacksToRemove = currentStacks.stream()
+                .filter(stack -> !stacksId.contains(stack))
                 .toList();
 
         // Create MemberStackEntity to add
         List<MemberStackEntity> memberStackEntitiesToAdd = stacksToAdd.stream()
-                .map(stackId -> MemberStackEntity.builder()
+                .map(stack -> MemberStackEntity.builder()
                         .member(member)
-                        .techStack(techStackRepository.findById(stackId)
-                                .orElseThrow(() -> new IllegalArgumentException("No stack exists"))
-                        )
+                        .techStack(stack)
                         .build()
                 )
                 .collect(Collectors.toList());
@@ -249,14 +238,17 @@ public class MemberService {
 
         // Get MemberStackEntity to remove
         List<MemberStackEntity> memberStackEntitiesToRemove = stacksToRemove.stream()
-                .map(stackId -> memberStackRepository.findByMemberIdAndTechStackId(memberId, stackId)
-                        .orElseThrow(() -> new IllegalArgumentException("No stack"))
+                .map(stack -> MemberStackEntity.builder()
+                        .member(member)
+                        .techStack(stack)
+                        .build()
                 )
                 .collect(Collectors.toList());
+
         // Delete stacks
         memberStackRepository.deleteAllInBatch(memberStackEntitiesToRemove);
 
-        return memberStackRepository.findByMemberId(memberId).stream()
+        return memberStackRepository.findByMemberId(member.getId()).stream()
                 .map(memberStackEntity -> memberStackEntity.getTechStack().getName())
                 .toList();
     }
