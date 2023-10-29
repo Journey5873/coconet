051f6991b65ac7a3331c0eddaf0a4c8efa9f9d95
bkdragon0228 <rlaqjarb2067@naver.com>
2023-10-29 20:41:18 +0900

    feat : 내 관심글 지원 목록 탭 추가

--- src/pages/myLikes/index.tsx
+++ src/pages/myLikes/index.tsx
@@ -1,64 +1,84 @@
-import { useState } from "react";
-import { BiSolidBookBookmark, BiSolidHeart } from "react-icons/bi";
-import styled from "styled-components";
+import { useState } from 'react';
+import { BiSolidBookBookmark, BiSolidHeart } from 'react-icons/bi';
+import styled from 'styled-components';
 
-import Tabs from "../../components/organisms/customTabs/Tabs";
+import Tabs from '../../components/organisms/customTabs/Tabs';
 
-import Tab from "../../components/organisms/customTabs/Tab";
+import Tab from '../../components/organisms/customTabs/Tab';
 
 const MyLikes = () => {
-    const [isActive, setIsActive] = useState<boolean>(false);
+  const [isActive, setIsActive] = useState<boolean>(false);
 
-    const [value, setValue] = useState("one");
+  const [value, setValue] = useState('one');
 
-    const handleChange = (event: any, newValue: string) => {
-        setValue(newValue);
-    };
+  const handleChange = (event: any, newValue: string) => {
+    setValue(newValue);
+  };
 
-    return (
-        <StyledMyPostsPageWrapper>
-            <StyledMypostsMyLikes>
-                <StyledMypostsMain>
-                    <Tabs
-                        options={[
-                            {
-                                name: "읽은 목록",
-                                icon: <BiSolidBookBookmark color="#8CAF8E" style={{ width: 35, height: 35 }} />,
-                            },
-                            {
-                                name: "관심 목록",
-                                icon: <BiSolidHeart color="#8CAF8E" style={{ width: 35, height: 35 }} />,
-                            },
-                        ]}
-                    >
-                        <Tab value="읽은 목록">읽은 목록</Tab>
-                        <Tab value="관심 목록">관심 목록</Tab>
-                    </Tabs>
-                </StyledMypostsMain>
-            </StyledMypostsMyLikes>
-        </StyledMyPostsPageWrapper>
-    );
+  return (
+    <StyledMyPostsPageWrapper>
+      <StyledMypostsMyLikes>
+        <StyledMypostsMain>
+          <Tabs
+            options={[
+              {
+                name: '읽은 목록',
+                icon: (
+                  <BiSolidBookBookmark
+                    color="#8CAF8E"
+                    style={{ width: 35, height: 35 }}
+                  />
+                ),
+              },
+              {
+                name: '관심 목록',
+                icon: (
+                  <BiSolidHeart
+                    color="#8CAF8E"
+                    style={{ width: 35, height: 35 }}
+                  />
+                ),
+              },
+              {
+                name: '지원 목록',
+                icon: (
+                  <BiSolidHeart
+                    color="#8CAF8E"
+                    style={{ width: 35, height: 35 }}
+                  />
+                ),
+              },
+            ]}
+          >
+            <Tab value="읽은 목록">읽은 목록</Tab>
+            <Tab value="관심 목록">관심 목록</Tab>
+            <Tab value="지원 목록">지원 목록</Tab>
+          </Tabs>
+        </StyledMypostsMain>
+      </StyledMypostsMyLikes>
+    </StyledMyPostsPageWrapper>
+  );
 };
 export default MyLikes;
 
 const StyledMyPostsPageWrapper = styled.section`
-    padding-top: 5rem;
+  padding-top: 5rem;
 `;
 
 const StyledMypostsMyLikes = styled.div`
-    width: 1300px;
-    margin: 0 auto;
-    min-height: 100vh;
+  width: 1300px;
+  margin: 0 auto;
+  min-height: 100vh;
 
-    @media screen and (max-width: 1919px) {
-        width: 1200px;
-    }
-    @media screen and (max-width: 1024px) {
-        width: calc(100% - 2rem);
-    }
+  @media screen and (max-width: 1919px) {
+    width: 1200px;
+  }
+  @media screen and (max-width: 1024px) {
+    width: calc(100% - 2rem);
+  }
 `;
 
 const StyledMypostsMain = styled.main`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
