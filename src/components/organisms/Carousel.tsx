84ab3ed250e3980422132a47938b0f5435c4a3b3
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2023-11-14 22:20:20 +0900

    fix : Carousel 이미지 삭제

--- src/components/organisms/Carousel.tsx
+++ src/components/organisms/Carousel.tsx
@@ -1,47 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import Carousel from 'react-material-ui-carousel';
-
-import mainImage1 from '../assets/images/메인.png';
+import Carousel from "react-material-ui-carousel";
 
 const CustomCarousel = () => {
-  const imageList = [
-    {
-      title: 'main1',
-      url: mainImage1,
-    },
-  ];
-  return (
-    <StyledCarouselContainer>
-      <Carousel cycleNavigation={true} navButtonsAlwaysVisible={true}>
-        {imageList.map((content) => (
-          <StyledCarouselItem>
-            {/* <Typography variant="h3" color="#112b23">
+    const imageList = [
+        {
+            title: "main1",
+            url: "",
+        },
+    ];
+    return (
+        <StyledCarouselContainer>
+            <Carousel cycleNavigation={true} navButtonsAlwaysVisible={true}>
+                {imageList.map((content) => (
+                    <StyledCarouselItem>
+                        {/* <Typography variant="h3" color="#112b23">
               {content.title}
             </Typography> */}
-            <StyledImage src={content.url} />
-          </StyledCarouselItem>
-        ))}
-      </Carousel>
-    </StyledCarouselContainer>
-  );
+                        <StyledImage src={content.url} />
+                    </StyledCarouselItem>
+                ))}
+            </Carousel>
+        </StyledCarouselContainer>
+    );
 };
 
 export default CustomCarousel;
 
 const StyledCarouselContainer = styled.div`
-  width: 1200px;
+    width: 1200px;
 `;
 
 const StyledCarouselItem = styled.div`
-  width: 100%;
-  aspect-ratio: 3 / 1;
+    width: 100%;
+    aspect-ratio: 3 / 1;
 `;
 
 const StyledImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `;
