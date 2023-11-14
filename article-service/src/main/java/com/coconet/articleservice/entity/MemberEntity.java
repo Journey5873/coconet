4fdf3b821d9e7355889e7a6c3dc6e88b61847098
inna <journeyin73@gmail.com>
2023-11-14 15:09:46 +0900

    Edit: Modify MemberEntity ID and UUID Column Names

--- article-service/src/main/java/com/coconet/articleservice/entity/MemberEntity.java
+++ article-service/src/main/java/com/coconet/articleservice/entity/MemberEntity.java
@@ -5,6 +5,7 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
+import java.util.UUID;
 
 import static lombok.AccessLevel.*;
 
@@ -19,7 +20,7 @@ public class MemberEntity extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "member_id")
     private Long id;
 
     @Column(length = 50, nullable = false)
@@ -27,4 +28,13 @@ public class MemberEntity extends BaseEntity{
 
     @Column(length = 50, nullable = false)
     private String name;
+
+    @Column(name = "member_uuid")
+    private UUID memberUUID;
+
+    public MemberEntity(Long id, String email, String name){
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
 }
