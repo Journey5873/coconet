fcad53e6283103ca7352dd7900c8015fc14bcb33
inna <journeyin73@gmail.com>
2023-12-07 17:24:19 +0900

    Edit: add new fields

--- member-service/src/main/java/com/coconet/memberservice/dto/MemberIdDto.java
+++ member-service/src/main/java/com/coconet/memberservice/dto/MemberIdDto.java
@@ -3,8 +3,19 @@ package com.coconet.memberservice.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 @AllArgsConstructor
 @Getter
 public class MemberIdDto {
-    private Long id;
+    private String email;
+    private String name;
+    private UUID memberUUID;
+    private String profileImage;
+    private List<MemberRoleResponse> roles;
+    private List<MemberStackResponse> stacks;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
