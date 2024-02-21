bf9836fe8999264064e756900e1ee2545824bfe5
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2024-02-21 20:50:32 +0900

    feat : 로그인 버튼 인터렉션 추가

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -74,7 +74,7 @@ export default function Header() {
           )}
           {!token && (
             <StyledPostRegister
-              onClick={() => () =>
+              onClick={() =>
                 setOpenLoginModal((openLoginModal) => !openLoginModal)
               }
             >
