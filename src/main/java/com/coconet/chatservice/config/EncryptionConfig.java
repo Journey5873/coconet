f27bfcbd582e2d42fd989086776e02780a8783c1
Jeremy <jamkim1997@gmail.com>
2024-03-10 20:01:50 +1100

    Add : Encryption added

---
+++ src/main/java/com/coconet/chatservice/config/EncryptionConfig.java
@@ -0,0 +1,58 @@
+package com.coconet.chatservice.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+@Configuration
+@EnableEncryptableProperties
+public class EncryptionConfig {
+    @Value("${jasypt.encryptor.algorithm}")
+    private String algorithm;
+    @Value("${jasypt.encryptor.pool-size}")
+    private int poolSize;
+    @Value("${jasypt.encryptor.string-output-type}")
+    private String stringOutputType;
+    @Value("${jasypt.encryptor.key-obtention-iterations}")
+    private int keyObtentionIterations;
+
+    @Bean
+    public StringEncryptor jasyptStringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setPoolSize(poolSize);
+        encryptor.setAlgorithm(algorithm);
+        encryptor.setPassword(getJasyptEncryptorPassword());
+        encryptor.setStringOutputType(stringOutputType);
+        encryptor.setKeyObtentionIterations(keyObtentionIterations);
+        return encryptor;
+    }
+
+    private String getJasyptEncryptorPassword() {
+        try {
+            ClassPathResource resource = new ClassPathResource("chat-encryptor-password.txt");
+            InputStream inputStream = resource.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+
+            StringBuilder builder = new StringBuilder();
+            while (true) {
+                String line = br.readLine();
+                if(line == null ) break;
+                builder.append(line);
+            }
+
+            return builder.toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Not found Jasypt password file.");
+        }
+    }
+}
